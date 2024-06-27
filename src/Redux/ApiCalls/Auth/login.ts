a5b1328522c8a5eabf52b1bada280a3f352bc097
DhruvTCS <dhruvgopani27@gmail.com>
2024-06-27 12:28:04 +0530

    add backend Url

--- src/Redux/ApiCalls/Auth/login.ts
+++ src/Redux/ApiCalls/Auth/login.ts
@@ -6,7 +6,7 @@ export const loginUser = createAsyncThunk(
     'auth/loginUser',
     async (credentials: { phone_number: string }, { rejectWithValue }) => {
         try {
-            const response = await axios.post('/account/sendotp/', credentials);
+            const response = await axios.post('https://photo-app-be-python.onrender.com/account/sendotp/', credentials);
             console.log(response.data);
             return response.data;
         } catch (error: any) {
@@ -18,7 +18,7 @@ export const verifyOTP = createAsyncThunk(
     'auth/verifyOTP',
     async (credentials: { country_code: string, phone_number: string, otp: string, orderId: string }, { rejectWithValue }) => {
         try {
-            const response = await axios.post('/account/login/', credentials);
+            const response = await axios.post('https://photo-app-be-python.onrender.com/account/login/', credentials);
             console.log(response.data);
             return response.data;
         } catch (error: any) {
@@ -47,7 +47,7 @@ export const resendOTP = createAsyncThunk(
     'auth/resendOTP',
     async (credentials: { orderId: string }, { rejectWithValue }) => {
         try {
-            const response = await axios.post('/account/resent-otp/', credentials);
+            const response = await axios.post('https://photo-app-be-python.onrender.com/account/resent-otp/', credentials);
             return response.data;
         } catch (error: any) {
             return rejectWithValue(error.response.data);
